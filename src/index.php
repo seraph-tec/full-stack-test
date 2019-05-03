48a8a8642acb50e17731e20ca4bb6fb92915c92b
Mateus Auler <mateusaulerlima@gmail.com>
2019-05-03 11:13:44 -0300

    Adicionado menu

--- src/index.php
+++ src/index.php
@@ -1,9 +1,28 @@
-<html>
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="/css/bootstrap.css">
+	
+	<link rel="stylesheet" href="/css/menu.css">
+
+	<script src="/js/jquery.js"></script>
+	<script src="/js/bootstrap.js"></script>
+
+	<title>Teste full-stack</title>
+
 </head>
 
 <body>
+
+		<?php require_once 'menu.php' ?>
+
 </body>
 
 </html>
