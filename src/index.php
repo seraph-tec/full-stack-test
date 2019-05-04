2230903b38b4ce48811b61152f486ac19beef9df
Mateus Auler <mateusaulerlima@gmail.com>
2019-05-03 23:35:38 -0300

    Títulos

--- src/index.php
+++ src/index.php
@@ -6,10 +6,11 @@
 
 <head>
 	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="/css/bootstrap.css">
 	
+	<link rel="stylesheet" href="/css/index.css">
 	<link rel="stylesheet" href="/css/menu.css">
 
 	<script src="/js/jquery.js"></script>
@@ -20,8 +21,26 @@
 </head>
 
 <body>
-
+	<div class="container-fluid">
 		<?php require_once 'menu.php' ?>
+		
+		<div id="link-form" class="row justify-content-center align-items-center">
+			<div>
+				<div class="col-12">
+					<h1 class="text-center">Título principal</h1>
+				</div>
+				<div class="col-12">
+					<h4 class="text-center">Título secundário</h4>
+				</div>
+				<div class="col-12">
+					<div class="text-center">
+						<a class="btn btn-primary" href="#form">Clique aqui</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
 
 </body>
 
